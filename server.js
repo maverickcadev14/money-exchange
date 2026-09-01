b14c6aeb4530c71184ba8bf38390e5faec3f741f
maverickcadev14 <maverick.ca@hotmail.com>
2026-09-01 19:37:21 +0800

    Add files via upload

--- server.js
+++ server.js
@@ -12,8 +12,10 @@ const contentTypes = {
   '.html': 'text/html; charset=utf-8',
   '.css': 'text/css; charset=utf-8',
   '.js': 'text/javascript; charset=utf-8',
+  '.png': 'image/png',
   '.svg': 'image/svg+xml',
-  '.json': 'application/json; charset=utf-8'
+  '.json': 'application/json; charset=utf-8',
+  '.webmanifest': 'application/manifest+json; charset=utf-8'
 };
 
 function sendJson(response, statusCode, body) {
